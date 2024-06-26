b61a8a4a3f3625f8991e37296f82a8a40c4992d0
Ahsan Ali <118122103+ahsanali890@users.noreply.github.com>
2024-06-27 01:14:13 +0500

    new changes

--- App.js
+++ App.js
@@ -1,34 +1,80 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React, {useEffect} from 'react';
+* Sample React Native App
+* https://github.com/facebook/react-native
+*
+* @format
+* @flow strict-local
+*/
+ 
+import React, {useEffect, useState, useRef} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  ActivityIndicator,
+  ScrollView,
+  RefreshControl,
   View,
 } from 'react-native';
 import {WebView} from 'react-native-webview';
 import SplashScreen from 'react-native-splash-screen';
-
+ 
 const App: () => Node = () => {
+  const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const webViewRef = useRef(null);
+  const onRefresh = () => {
+    setRefreshing(true);
+    if (webViewRef.current) {
+      webViewRef.current.reload();
+    }
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1500);
+  };
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
     }, 2000);
   }, []);
   return (
-    <SafeAreaView style={styles.container}>
-      <WebView source={{uri: 'https://chloeconor.com/'}} style={{flex: 1}} />
+    <SafeAreaView style={{flex: 1}}>
+      {loading && (
+        <ActivityIndicator
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 2,
+            backgroundColor: 'white',
+          }}
+          size="large"
+          color="#401E02"
+        />
+      )}
+      <ScrollView
+        contentContainerStyle={{flex: 1}}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={['#401E02']}
+          />
+        }>
+        <WebView
+          ref={webViewRef}
+          source={{uri: 'https://chloeconor.com/'}}
+          style={{
+            flex: 1,
+            background: 'transparent',
+            backgroundColor: 'transparent',
+            opacity: loading ? 0 : 1,
+          }}
+          onLoad={() => setLoading(false)}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
